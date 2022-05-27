0271160ce8c64fd84fa49a375a93ee8d81cd8ac1
arjun <arjun259194@gmail.com>
2022-05-27 16:23:52 +0530

    event error

--- app.js
+++ app.js
@@ -2,8 +2,31 @@ console.log("Connected");
 
 const textArea = document.querySelector('#input-text');
 const command = document.querySelector('#command');
+const terminal = document.querySelector('#terminal');
 
 window.addEventListener("keyup",()=>{
   command.innerHTML = textArea.value;
   textArea.focus();
 })
+
+//!Error
+textArea.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    runCommand(textArea.value);  
+    textArea.value = "";
+  }
+})
+
+//!Error
+function runCommand(command) {
+  console.log("🚀 ~ file: app.js ~ line 22 ~ runCommand ~ textArea.value", textArea.value)
+  switch (command.toLowerCase()) {
+    case "help":
+      terminal.innerHTML += `<p>You will get help</p>`
+      break;
+  
+    default:
+      terminal.innerHTML = `<p>error try again</p>`
+      break;
+  }
+}
